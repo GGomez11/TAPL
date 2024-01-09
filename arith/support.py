41a94d61feb7f59127c71c444af322db66bb84eb
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-01-08 22:02:34 -0600

    create file info helper

---
+++ arith/support.py
@@ -0,0 +1,12 @@
+class FileInfo:
+    def __repr__(self):
+        return 'Uknown file and line>:'
+
+class Info(FileInfo):
+    def __init__(self, file_name, line_num, char_pos):
+        self._file_name = file_name
+        self._line_num = line_num
+        self._char_pos = char_pos
+
+    def __repr__(self):
+        return f'{self._file_name}: {self._line_num}.{self._char_pos}:'
